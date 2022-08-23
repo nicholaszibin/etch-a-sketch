22f4e9278b8304327d2259dfc965c6e2f69a783a
nicholaszibin <nicholaszibin@gmail.com>
2022-08-22 22:03:55 -0700

    Progress on flex alignment and creating the drawing area with slider

--- javascript.js
+++ javascript.js
@@ -1,11 +1,18 @@
+
 const grid = document.querySelector('.grid');
-console.log(grid.clientWidth);
 
 let numberGrid = 4;
-for (let i = 0; i < numberGrid; i++) {
-    const squares = document.createElement('div');
-    squares.classList.add('squares');
-    //squares.style.border = 'solid';
-    squares.style.width = `${grid.clientWidth/Math.sqrt(numberGrid)}px`;
-    grid.appendChild(squares);
-};
+
+function updateTextInput(val) {
+    document.getElementById('textInput').value=val;
+    let numberGrid = val;
+    
+    for (let i = 0; i < Math.pow(numberGrid,2); i++) {
+        const squares = document.createElement('div');
+        squares.classList.add('squares');
+        //squares.style.border = 'solid';
+        squares.style.width = `${grid.clientWidth/numberGrid}px`;
+        grid.appendChild(squares);
+    };
+
+  }
